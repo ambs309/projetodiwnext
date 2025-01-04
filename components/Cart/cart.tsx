e9ec9711cb7564cde8b5c4375cb507741c0d1b38
André Sobreira - a22305703 <160252318+ambs309@users.noreply.github.com>
2025-01-04 22:12:55 +0000

    nubai

--- components/Cart/cart.tsx
+++ components/Cart/cart.tsx
@@ -3,6 +3,7 @@
 import React, { useState, useEffect } from 'react';
 import { Product } from '@/app/models/interfaces';
 
+
 export default function Cart() {
   const [cartItems, setCartItems] = useState<Product[]>([]);
 
